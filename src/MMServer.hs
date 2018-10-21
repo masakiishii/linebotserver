142b88dbc8a2bb12c27ff4d2cbfbaef98154c6f8
masakiishii <masaki.ishii511@gmail.com>
2018-10-21 19:16:48 +0900

    remove unnecessary code

--- src/MMServer.hs
+++ src/MMServer.hs
@@ -49,20 +49,6 @@ import Network.HTTP.Client.TLS (tlsManagerSettings)
 import KeyReader
 import Executor
 
-
-data Position = Position
-  { xCoord :: Int
-  , yCoord :: Int
-  } deriving Generic
-
-instance ToJSON Position
-
--- [API] -----------------------------------------------------------------------------------------------
-type API = "position" :> Capture "x" Int :> Capture "y" Int :> Get '[JSON] Position
-      :<|> "hello" :> QueryParam "name" String :> Get '[JSON] HelloMessage
-      -- :<|> "marketing" :> ReqBody '[JSON] ClientInfo :> Post '[JSON] Email
-      :<|> "marketing" :> ReqBody '[JSON] ClientInfo :> Post '[JSON] NoContent
-
 type LineReceiveAPI = "webhook"
                 :> Header "X-Line-Signature" T.Text
                 :> ReqBody '[JSON] ReceiveHook
@@ -73,69 +59,12 @@ type LineSendAPI = "v2" :> "bot" :> "message" :> "reply"
                 :> Header "Authorization" T.Text
                 :> ReqBody '[JSON] SendHook
                 :> Post '[JSON] NoContent
---------------------------------------------------------------------------------------------------------
 
-
-newtype HelloMessage = HelloMessage { msg :: String }
-  deriving Generic
-
-instance ToJSON HelloMessage
-
-data ClientInfo = ClientInfo
-  { clientName :: String
-  , clientEmail :: String
-  , clientAge :: Int
-  , clientInterestedIn :: [String]
-  } deriving Generic
-
-instance FromJSON ClientInfo
-instance ToJSON ClientInfo
-
-data Email = Email
-  { from :: String
-  , to :: String
-  , subject :: String
-  , body :: String
-  } deriving Generic
-
-instance ToJSON Email
-
-emailForClient :: ClientInfo -> Email
-emailForClient c = Email from' to' subject' body'
-
-  where from'    = "great@company.com"
-        to'      = clientEmail c
-        subject' = "Hey " ++ clientName c ++ ", we miss you!"
-        body'    = "Hi " ++ clientName c ++ ",\n\n"
-                ++ "Since you've recently turned " ++ show (clientAge c)
-                ++ ", have you checked out our latest "
-                ++ intercalate ", " (clientInterestedIn c)
-                ++ " products? Give us a visit!"
-
-server3 :: Int -> Server API
-server3 num = position
-     :<|> hello
-     :<|> marketing
-
-  where position :: Int -> Int -> Handler Position
-        position x y = return (Position num y)
-
-        hello :: Maybe String -> Handler HelloMessage
-        hello mname = return . HelloMessage $ case mname of
-          Nothing -> "Hello, anonymous coward"
-          Just n  -> "Hello, " ++ n
-
-        marketing :: ClientInfo -> Handler NoContent
-        -- marketing clientinfo = return (emailForClient clientinfo)
-        marketing clientinfo = return NoContent
-
------------------------------------------------------------------
 lineSendApi :: Proxy LineSendAPI
 lineSendApi = Proxy
 
 putMessage :: Maybe T.Text -> Maybe T.Text -> SendHook ->  ClientM NoContent
 putMessage = client lineSendApi
------------------------------------------------------------------
 
 makeBody :: ReceiveHook -> SendHook
 makeBody receiveHook =
@@ -167,18 +96,11 @@ lineReceiveServer manager lineToken = receiveMessage
             liftIO $ print $ Data.Receive.EventData.replyToken evHead
             liftIO $ print $ Data.Receive.EventData.typeString evHead
             liftIO $ print $ Data.Receive.EventData.timestamp evHead
-            -- let a = execute manager $! putMessage (Just "application/json") (Just lineToken) (makeBody receiveHook)
             liftIO $ executeReply manager lineToken receiveHook
             return NoContent
 
-userAPI3 :: Proxy API
-userAPI3 = Proxy
-
 lineReceiveApi :: Proxy LineReceiveAPI
 lineReceiveApi = Proxy
-
-app3 :: Int -> Application
-app3 num = serve userAPI3 (server3 num)
 
 lineApplication :: Manager -> T.Text -> Application
 lineApplication manager lineToken = serve lineReceiveApi (lineReceiveServer manager lineToken)
@@ -191,6 +113,4 @@ bootServer = do
       lineToken = T.pack $ "Bearer " ++ keys!!0
       channelSecretKey = T.pack $ keys!!1
   run 8081 (lineApplication manager lineToken)
-  -- func manager accessKey secretKey lineToken
-  -- run 8081 (app3 3)
 
