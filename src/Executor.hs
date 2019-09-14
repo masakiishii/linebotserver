5fb7fcccc0dc40c1c012009bd0c8d6f3679e0d76
masakiishii <masaki.ishii511@gmail.com>
2019-09-14 16:51:35 +0900

    update lts version

--- src/Executor.hs
+++ src/Executor.hs
@@ -3,8 +3,8 @@ module Executor where
 import Network.HTTP.Client (Manager)
 import Servant.Client
 
-execute :: Manager -> ClientM a -> IO (Either ServantError a)
+execute :: Manager -> ClientM a -> IO (Either ClientError a)
 execute manager client = runClientM client $ mkClientEnv manager $ BaseUrl Https "api.line.me" 443 ""
 
-executeBit :: Manager -> ClientM a -> IO (Either ServantError a)
-executeBit manager client = runClientM client $ mkClientEnv manager $ BaseUrl Https "api.bitflyer.jp" 443 ""
+executeBit :: Manager -> ClientM a -> IO (Either ClientError a)
+executeBit manager client = runClientM client $ mkClientEnv manager $ BaseUrl Https "api.bitflyer.com" 443 ""
